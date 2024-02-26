9a133d715a0ce18d3aa53a14d89dadf1089a7edc
karkar-div <karkarcomp@gmail.com>
2024-02-27 00:08:35 +0300

    more testing and lex fix
    Signed-off-by: karkar-div <karkarcomp@gmail.com>

--- src/VertualMachine.cpp
+++ src/VertualMachine.cpp
@@ -7,42 +7,40 @@
 #include "Instructions.cpp"
 #include "Dependencies.cpp"
 
-union address_as_int{
-    int64_t value;
-    int64_t* address;
-};
-
-
 class VirtualMachine{
-	public:
-        int _stack_size;
+	private:
+		union address_as_int{
+			int64_t value;
+			int64_t* address;
+		};
+		int _stack_size;
 		int64_t* Registers;
 		int64_t* Stack;
-        int64_t* StackPointer;
-        int64_t* BasePointer;
-        int64_t parameter_value(Parameter* param){
-            switch(param->Register){
-                case Null:
-                    return param->Offset;
-                case SP:
-                    return (int64_t)StackPointer;
-                case BP:
-                    return (int64_t)BasePointer;
-                default:
-                    return Registers[param->Register];
-            }
-        }
-        int64_t* destenation(Parameter param){
-            switch(param.Register){
-                case SP:
-                    return &((int64_t&)StackPointer);
-                case BP:
-                    return &((int64_t&)BasePointer);
-                default:
-                    return &Registers[param.Register];
-            }
-        }
-        int64_t binary_operator(InstructionType type,int64_t value1,int64_t value2){
+		int64_t* StackPointer;
+		int64_t* BasePointer;
+		int64_t parameter_value(Parameter* param){
+			switch(param->Register){
+				case Null:
+					return param->Offset;
+				case SP:
+					return (int64_t)StackPointer;
+				case BP:
+					return (int64_t)BasePointer;
+				default:
+					return Registers[param->Register];
+			}
+		}
+		int64_t* destenation(Parameter param){
+			switch(param.Register){
+				case SP:
+					return &((int64_t&)StackPointer);
+				case BP:
+					return &((int64_t&)BasePointer);
+				default:
+					return &Registers[param.Register];
+			}
+		}
+		int64_t binary_operator(InstructionType type,int64_t value1,int64_t value2){
 			switch(type){
 				case Add  : return  value1 +  value2;
 				case Sub  : return  value1 -  value2;
@@ -58,10 +56,11 @@ class VirtualMachine{
 			}
 			return 0;
 		}
+	public:
 		VirtualMachine(int64_t entrypoint,int64_t stack_size = 0x100000,int64_t registers_num = 20){
-            Stack = new int64_t[stack_size];
+			Stack = new int64_t[stack_size];
 			memset(Stack,0,sizeof(Stack));
-            _stack_size = stack_size;
+			_stack_size = stack_size;
 			Registers = new int64_t[registers_num];
 			memset(Registers,0,sizeof(int64_t)*registers_num);
 			StackPointer = &Stack[stack_size-1];
@@ -72,16 +71,16 @@ class VirtualMachine{
 			while(true){
 				Instruction* instruction = (*instructions)[Registers[RegisterType::IP]];
 				if(debug){
-                    printf("[");
-                    for(int x = 0;x < 10;x++){
-                        printf(" ");
-                        if(StackPointer == &Stack[_stack_size - x])
-                            printf("SP:");
-                        if(BasePointer == &Stack[_stack_size - x])
-                            printf("BP:");
-                        printf("%d ",Stack[_stack_size - x]);
-                    }
-                    printf("]\n");
+					printf("[");
+					for(int x = 0;x < 10;x++){
+						printf(" ");
+						if(StackPointer == &Stack[_stack_size - x])
+							printf("SP:");
+						if(BasePointer == &Stack[_stack_size - x])
+							printf("BP:");
+						printf("%d ",Stack[_stack_size - x]);
+					}
+					printf("]\n");
 					printf("AX:%d ",Registers[RegisterType::AX]);
 					printf("IP:%d ",Registers[RegisterType::IP]);
 					printf("SP:%d ",StackPointer);
@@ -90,60 +89,60 @@ class VirtualMachine{
 				switch (instruction->Type){
 					/* memory control */
 					case drfrnc:{
-                        address_as_int stack_top;
-                        stack_top.value = *StackPointer;
-                        *StackPointer = *(stack_top.address);
-                        break;
-                    }
+						address_as_int stack_top;
+						stack_top.value = *StackPointer;
+						*StackPointer = *(stack_top.address);
+						break;
+					}
 					/* control flow */
 					case Jmp:{
 						Registers[RegisterType::IP] = parameter_value(&(instruction->Parameters[FIRST]));
 						Registers[RegisterType::IP]--;
 						break;
-                    }
+					}
 					case Nop:{
 						break;                        
-                    }
+					}
 					case JN:{
-                        if(Registers[RegisterType::CR] == false){
+						if(Registers[RegisterType::CR] == false){
 							Registers[RegisterType::IP] = parameter_value(&(instruction->Parameters[FIRST]));
 							Registers[RegisterType::IP]--;
 						}
 						break;
-                    }
+					}
 					/* Stack minipulation (not that the Stack grows upowrd)*/
 					case Push:{
 						StackPointer--;
 						*StackPointer = parameter_value(&(instruction->Parameters[FIRST]));
 						break;                        
-                    }
+					}
 					case Pop:{
 						if(instruction->ParametersNum == 1)
 							*(destenation(instruction->Parameters[FIRST])) = *StackPointer;
 						StackPointer++;
 						break;                        
-                    }
+					}
 					case Mov:
 						if(instruction->ParametersNum == 2)
 							*(destenation(instruction->Parameters[FIRST])) = parameter_value(&(instruction->Parameters[SECOND]));
 						if(instruction->ParametersNum == 0){
-                            address_as_int temp;
-                            temp.value = *(StackPointer+1);
-                            *temp.address = *StackPointer;
-                            StackPointer++;
+							address_as_int temp;
+							temp.value = *(StackPointer+1);
+							*temp.address = *StackPointer;
+							StackPointer++;
 							//Stack[Stack[Registers[RegisterType::SP]+1]] = Stack[Registers[RegisterType::SP]];
 							//StackPointer++;
 						}
 						break;
 					case Exit:{
 						return;                        
-                    }
+					}
 					case so_call:{
 						// TODO ??????
 						std::list<int64_t>* args = new std::list<int64_t>;
 						for(int x = 0; x < parameter_value(&(instruction->Parameters[SECOND])) ;x++){
 							args->push_back(*StackPointer);
-						    StackPointer++;
+							StackPointer++;
 						}
 						(*Dependencies)[parameter_value(&(instruction->Parameters[FIRST]))]->Run(args);
 						break;
